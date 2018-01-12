d5d386fa09926e2919f77b1954a92b62ff12d53e
dbsibmpoc1 <35222259+dbsibmpoc1@users.noreply.github.com>
2018-01-12 17:48:04 +0530

    Updated with comments

--- vm.tf
+++ vm.tf
@@ -17,16 +17,19 @@
 # Create security group
 ##############################################################################
 
+# Create security group for web server instances
 resource "ibm_security_group" "dbsystelsg" {
     name = "dbsystel-sg"
     description = "setup security group for dbsystel apps"
 }
 
+# Create security group for DB server instance
 resource "ibm_security_group" "dbsysteldbsg" {
     name = "dbsystel-db-sg"
     description = "setup security group for dbsystel db"
 }
 
+# Security rule for allowing MySQL DB Connections
 resource "ibm_security_group_rule" "allow_db_port_3306" {
     direction = "ingress"
     ether_type = "IPv4"
@@ -36,6 +39,7 @@ resource "ibm_security_group_rule" "allow_db_port_3306" {
     security_group_id = "${ibm_security_group.dbsysteldbsg.id}"
 }
 
+# Security rule for allowing SSH connections to the DB VSI
 resource "ibm_security_group_rule" "allow_ssh_access_db" {
     direction = "ingress"
     ether_type = "IPv4"
@@ -45,6 +49,7 @@ resource "ibm_security_group_rule" "allow_ssh_access_db" {
     security_group_id = "${ibm_security_group.dbsysteldbsg.id}"
 }
 
+# Security rule for allowing access to web application port
 resource "ibm_security_group_rule" "allow_app_port_8080" {
     direction = "ingress"
     ether_type = "IPv4"
@@ -54,6 +59,7 @@ resource "ibm_security_group_rule" "allow_app_port_8080" {
     security_group_id = "${ibm_security_group.dbsystelsg.id}"
 }
 
+# Security rule for allowing SSH connections to the Web VSIs
 resource "ibm_security_group_rule" "allow_ssh_access" {
     direction = "ingress"
     ether_type = "IPv4"
@@ -63,6 +69,7 @@ resource "ibm_security_group_rule" "allow_ssh_access" {
     security_group_id = "${ibm_security_group.dbsystelsg.id}"
 }
 
+# Security rule for outbound connectivity to all VSIs
 data "ibm_security_group" "allow_outbound" {
     name = "allow_outbound"
 }
@@ -71,12 +78,14 @@ data "ibm_security_group" "allow_outbound" {
 # Create Resources
 ##############################################################################
 
+# Create a SSH Key
 resource "ibm_compute_ssh_key" "ssh_key" {
     label = "${var.ssh_label}"
     notes = "${var.ssh_notes}"
     public_key = "${var.ssh_key}"
 }
 
+# Rendering template for providing MySQL DB IP address to the Web Server instances
 data "template_file" "init" {
   template = "${file("install_apache.tpl")}"
 
@@ -85,8 +94,11 @@ data "template_file" "init" {
   }
 }
 
+# Create Web virtual server instances
 resource "ibm_compute_vm_instance" "dbs-web" {
+  # Variable count determines the number of web server instances to be instantiated
   count                    = "${var.webinstances}"
+  
   hostname                 = "${var.appname}-web-${format("%02d", count.index + 1)}"
   domain                   = "${var.domain}"
   os_reference_code        = "${var.os_reference_code}"
@@ -99,14 +111,18 @@ resource "ibm_compute_vm_instance" "dbs-web" {
   disks                    = ["${var.disk_size}"]
   local_disk               = false
   dedicated_acct_host_only = true
-  ssh_key_ids              = ["${ibm_compute_ssh_key.ssh_key.id}"]
   tags                     = ["${var.tags}"]
   user_metadata            = "${data.template_file.init.rendered}"
 
+  # Associate SSH Key for accessing the VSI. Can be associated with multiple SSH Keys.
+  ssh_key_ids              = ["${ibm_compute_ssh_key.ssh_key.id}"]
+
+  # Associate security groups
   private_security_group_ids = ["${ibm_security_group.dbsystelsg.id}", "${data.ibm_security_group.allow_outbound.id}"]
   public_security_group_ids = ["${ibm_security_group.dbsystelsg.id}", "${data.ibm_security_group.allow_outbound.id}"]
 }
 
+# Create a DB virtual server instance
 resource "ibm_compute_vm_instance" "dbs-db" {
   hostname                 = "${var.appname}-db"
   domain                   = "${var.domain}"
@@ -120,22 +136,22 @@ resource "ibm_compute_vm_instance" "dbs-db" {
   disks                    = ["${var.disk_size}"]
   local_disk               = false
   dedicated_acct_host_only = true
-  ssh_key_ids              = ["${ibm_compute_ssh_key.ssh_key.id}"]
   tags                     = ["${var.tags}"]
   user_metadata            = "${file("install_mysql.yml")}"
 
+  # Associate SSH Key for accessing the VSI. Can be associated with multiple SSH Keys.
+  ssh_key_ids              = ["${ibm_compute_ssh_key.ssh_key.id}"]
+  
+  # Associate security groups
   private_security_group_ids = ["${ibm_security_group.dbsysteldbsg.id}", "${data.ibm_security_group.allow_outbound.id}"]
   public_security_group_ids = ["${ibm_security_group.dbsysteldbsg.id}", "${data.ibm_security_group.allow_outbound.id}"]
 }
 
-# data "template_file" "web_list" {
-#   # count = "${var.webinstances}"
-#   template = "{\"private_ip_address\" = \"10.0.0.1\"}"
-# }
-
+# Create a Load Balancer routing traffic to the provisioned web server instances
 resource "ibm_lbaas" "lbaas" {
   name        = "${var.appname}-lb"
   description = "Load balancer for ${var.appname}"
+  # Uses the default (primary) private subnet for provisioning the load balancer
   subnets     = [1610317]
 
   protocols = [
@@ -148,8 +164,7 @@ resource "ibm_lbaas" "lbaas" {
     }
   ]
 
-  # server_instances = "${data.template_file.web_list.rendered}"
-
+  # Associate the web server instances to the load balancer for routing traffic
   server_instances = [
     {
       "private_ip_address" = "${ibm_compute_vm_instance.dbs-web.0.ipv4_address_private}"
@@ -160,6 +175,13 @@ resource "ibm_lbaas" "lbaas" {
   ]
 }
 
+##############################################################################
+# Output
+##############################################################################
+
+# Output IP Address of the load balancer for application access
 output "lb_id" {
-    value = ["http://${ibm_lbaas.lbaas.vip}:8080"]
+    value = ["http://${ibm_lbaas.lbaas.vip}:8080/DBSystelClusterApp/index.jsp"]
 }
+
+# End
